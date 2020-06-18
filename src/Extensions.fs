b657233f69a216df956f091638aa4810d9cca6c4
Andrii Dariienko <andrii.dariienko@outlook.com>
2020-06-18 15:08:13 +0300

    ExIm | Create solution for exporting/importing environment variables.

---
+++ src/Extensions.fs
@@ -0,0 +1,9 @@
+﻿module Extensions
+
+open System
+open System.Collections
+
+let objectToHashtable (obj : Object) =
+   match obj with
+   | :? Hashtable as hashtable -> hashtable
+   | _ -> invalidOp "Could not cast the object read from the file to Hashtable"
---
+++ src/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.Linq
+open Arguments
+open Performer
+
+[<EntryPoint>]
+let main argv =
+    let fileName =
+        match (getFileName argv).SingleOrDefault() with
+        | x when x <> null -> x
+        | x when x = null -> invalidArg "filename" "Could not recognize filename."
+        | _ -> invalidArg "arguments" "Could not recognize filename."
+
+    let operation =
+        match (getOperation argv).SingleOrDefault() with
+        | x when x <> null -> x
+        | x when x = null -> invalidArg "task" "Could not recognize task type."
+        | _ -> invalidArg "arguments" "Could not recognize task type."
+
+    let target =
+        match (getTarget argv).SingleOrDefault() with
+        | "machine" -> EnvironmentVariableTarget.Machine
+        | "user" -> EnvironmentVariableTarget.User
+        | _ -> invalidArg "arguments" "Could not recognize target type."
+
+    performOperation operation fileName target
+
+    0
